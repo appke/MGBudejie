20556b2af5205fc379e4d6b4d2e984c7335f3dd5
muliang001 <anglimu@163.com>
2018-10-11 21:08:00 +0800

    update .gitignore

--- 百思不得姐/Classes/Other-其他/Controller/MGTabBarController.m
+++ 百思不得姐/Classes/Other-其他/Controller/MGTabBarController.m
@@ -8,9 +8,9 @@
 
 #import "MGTabBarController.h"
 #import "MGEssenceViewController.h"
-#import "MGNewViewController.h"
 #import "MGFriendTrendsViewController.h"
 #import "MGMeViewController.h"
+#import "MGNewViewController.h"
 #import "MGTabBar.h"
 
 #import "MGNavigationController.h"
@@ -21,57 +21,54 @@
 
 @implementation MGTabBarController
 
-
-+ (void)initialize
-{
++ (void)initialize {
+    // 设置颜色
     NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
-    attrs[NSFontAttributeName] = [UIFont systemFontOfSize:12];
+    attrs[NSFontAttributeName] = [UIFont systemFontOfSize:13];
     attrs[NSForegroundColorAttributeName] = [UIColor grayColor];
-    
+
+    // 设置字体
     NSMutableDictionary *selAttrs = [NSMutableDictionary dictionary];
-    //    selAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:12];
     selAttrs[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
-    
-    UITabBarItem *tabBarItem = [UITabBarItem appearance];
+
+    //    UITabBarItem *tabBarItem = [UITabBarItem appearance]; // 有坑
+    UITabBarItem *tabBarItem = [UITabBarItem appearanceWhenContainedIn:self, nil];
     [tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
     [tabBarItem setTitleTextAttributes:selAttrs forState:UIControlStateSelected];
-    
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-//    UIViewController *vc01 = [[UIViewController alloc] init];
-//    vc01.tabBarItem.title = @"精华";
-//    vc01.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
-//    UIImage *selImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
-//    selImage = [selImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-//    vc01.tabBarItem.selectedImage = selImage;
-    
+
+    //    UIViewController *vc01 = [[UIViewController alloc] init];
+    //    vc01.tabBarItem.title = @"精华";
+    //    vc01.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
+    //    UIImage *selImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
+    //    selImage = [selImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    //    vc01.tabBarItem.selectedImage = selImage;
+
     [self setupChildVc:[[MGEssenceViewController alloc] init] title:@"精华" image:@"tabBar_essence_icon" selectedImage:@"tabBar_essence_click_icon"];
-    
+
     [self setupChildVc:[[MGNewViewController alloc] init] title:@"新帖" image:@"tabBar_new_icon" selectedImage:@"tabBar_new_click_icon"];
-    
+
     [self setupChildVc:[[MGFriendTrendsViewController alloc] init] title:@"关注" image:@"tabBar_friendTrends_icon" selectedImage:@"tabBar_friendTrends_click_icon"];
-    
+
     [self setupChildVc:[[MGMeViewController alloc] initWithStyle:UITableViewStyleGrouped] title:@"我的" image:@"tabBar_me_icon" selectedImage:@"tabBar_me_click_icon"];
-    
+
     // 更换tabBar
-//    self.tabBar = [[MGTabBar alloc] init];
+    //    self.tabBar = [[MGTabBar alloc] init];
     [self setValue:[[MGTabBar alloc] init] forKey:@"tabBar"];
 }
 
-
-- (void)setupChildVc:(UIViewController *)vc title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage{
-    
+- (void)setupChildVc:(UIViewController *)vc title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage {
     vc.title = title;
     vc.tabBarItem.image = [UIImage imageNamed:image];
     vc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    
-//    vc.view.backgroundColor = [UIColor colorWithRed:223/255.0 green:223/255.0 blue:223/255.0 alpha:1.0];
+
+    //    vc.view.backgroundColor = [UIColor colorWithRed:223/255.0 green:223/255.0 blue:223/255.0 alpha:1.0];
     MGNavigationController *nav = [[MGNavigationController alloc] initWithRootViewController:vc];
-    
+
     [self addChildViewController:nav];
 }
 
--- 百思不得姐/Classes/Publish-发布/View/MGAddTagToolbarView.m
+++ 百思不得姐/Classes/Publish-发布/View/MGAddTagToolbarView.m
@@ -21,86 +21,81 @@
 
 @implementation MGAddTagToolbarView
 
-- (NSMutableArray *)tagLabels
-{
+- (NSMutableArray *)tagLabels {
     if (!_tagLabels) {
         _tagLabels = [NSMutableArray array];
     }
     return _tagLabels;
 }
 
-- (void)awakeFromNib
-{
+- (void)awakeFromNib {
     // 添加一个按钮
     UIButton *addButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [addButton addTarget:self action:@selector(addButtonClick) forControlEvents:UIControlEventTouchUpInside];
-    
+
     [addButton setImage:[UIImage imageNamed:@"tag_add_icon"] forState:UIControlStateNormal];
-//    addButton.size = [UIImage imageNamed:@"tag_add_icon"].size;
-//    addButton.size = [addButton imageForState:UIControlStateNormal].size;
+    //    addButton.size = [UIImage imageNamed:@"tag_add_icon"].size;
+    //    addButton.size = [addButton imageForState:UIControlStateNormal].size;
     addButton.size = addButton.currentImage.size;
     addButton.x = MGTopicCellMargin;
-    
+
     [self.topView addSubview:addButton];
     _addButton = addButton;
-    
+
     // 默认标签
-//    [self createTags:@[@"吐槽吐槽吐槽吐槽", @"糗事吐槽吐槽吐槽吐槽", @"搞笑吐槽吐槽"]];
+    //    [self createTags:@[@"吐槽吐槽吐槽吐槽", @"糗事吐槽吐槽吐槽吐槽", @"搞笑吐槽吐槽"]];
     [self createTags:@[@"吐槽", @"糗事"]];
 }
 
 #pragma mark - push到添加标签控制器
-- (void)addButtonClick
-{
+- (void)addButtonClick {
     MGAddTagViewController *addTagVc = [[MGAddTagViewController alloc] init];
     __weak typeof(self) weakSelf = self;
     [addTagVc setTagsBlock:^(NSArray *tags) {
-        
+
         [weakSelf createTags:tags];
     }];
     addTagVc.tags = [self.tagLabels valueForKeyPath:@"text"];
-    
+
     UITabBarController *root = (UITabBarController *)[UIApplication sharedApplication].keyWindow.rootViewController;
-//    UINavigationController *nav = (UINavigationController *)[root selectedViewController];
-//    UIViewController *topVc = nav.topViewController;
+    //    UINavigationController *nav = (UINavigationController *)[root selectedViewController];
+    //    UIViewController *topVc = nav.topViewController;
     UINavigationController *postNav = (UINavigationController *)root.presentedViewController;
-    
+
     [postNav pushViewController:addTagVc animated:YES];
-    
-//    MGLog(@"%@\n%@\n%@", root, nav, topVc);
+
+    //    MGLog(@"%@\n%@\n%@", root, nav, topVc);
 }
 
--  (void)layoutSubviews
-{
+- (void)layoutSubviews {
     [super layoutSubviews];
-    
+
     for (int i = 0; i < self.tagLabels.count; i++) {
-        
         UILabel *tagLabel = self.tagLabels[i];
         // 排布label
         if (i == 0) { // 最前面的标签
             tagLabel.x = 0;
             tagLabel.y = 0;
         } else { // 其他标签按钮
-            UILabel *lastTagLabel = self.tagLabels[i-1];
-            
+            UILabel *lastTagLabel = self.tagLabels[i - 1];
+
             // 计算当前行左边的宽度
             CGFloat leftWidth = CGRectGetMaxX(lastTagLabel.frame) + MGTagMargin;
             // 计算当前行右边的宽度(剩余的宽度)
             CGFloat rightWidth = self.topView.width - leftWidth;
-            
+
             // 宽度够不够我用
             if (rightWidth >= tagLabel.width) {
                 tagLabel.x = leftWidth;
                 tagLabel.y = lastTagLabel.y;
-                
+
             } else { // 按钮显示在下一行
                 tagLabel.x = 0;
                 tagLabel.y = CGRectGetMaxY(lastTagLabel.frame) + MGTagMargin;
             }
         }
     }
-    
+
     // 拿到最后一个标签
     UILabel *lastLabel = [self.tagLabels lastObject];
     CGFloat leftWidth = CGRectGetMaxX(lastLabel.frame) + MGTagMargin;
@@ -112,38 +107,35 @@
         self.addButton.x = 0;
         self.addButton.y = CGRectGetMaxY([lastLabel frame]) + MGTagMargin;
     }
-    
+
     // 更新工具条的高度
     CGFloat oldH = self.height;
     self.height = CGRectGetMaxY(self.addButton.frame) + 40;
     self.y += oldH - self.height;
-    
 }
 
 #pragma mark - 创建标签
-- (void)createTags:(NSArray *)tags
-{
+- (void)createTags:(NSArray *)tags {
     [self.tagLabels makeObjectsPerformSelector:@selector(removeFromSuperview)];
     [self.tagLabels removeAllObjects];
-    
+
     for (int i = 0; i < tags.count; i++) {
-        
         UILabel *tagLabel = [[UILabel alloc] init];
         tagLabel.text = tags[i];
-        
+
         tagLabel.font = MGTagFont;
         tagLabel.backgroundColor = MGTagBg;
         tagLabel.textColor = [UIColor whiteColor];
         tagLabel.textAlignment = NSTextAlignmentCenter;
-        
+
         [tagLabel sizeToFit];
         tagLabel.width += 2 * MGTagMargin;
         tagLabel.height = MGTagH;
-        
+
         [self.tagLabels addObject:tagLabel];
         [self.topView addSubview:tagLabel];
     }
-    
+
     // 重新布局子控件
     [self setNeedsLayout];
 }
