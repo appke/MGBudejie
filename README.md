86cd3e99155d8bb5b34f3f41a803c83ee54464ad
muliang001 <anglimu@163.com>
2018-10-11 20:52:55 +0800

    进入初始化

--- README.md
+++ README.md
@@ -3,29 +3,29 @@
 
 - [x] 我的界面-点击方块-加载网页
 
-![](https://github.com/angmu/MGLove/blob/master/Screenshot/标签排布.gif)
+![](Screenshot/标签排布.gif)
 
 
 - [x] 我的界面-点击方块-加载网页
 
-![](https://github.com/angmu/MGLove/blob/master/Screenshot/我的方块.gif)
+![](Screenshot/我的方块.gif)
 
 
 - [x] 点击tabBar,刷新数据
 - 新帖控制器
 - 添加menuController
 
-![](https://github.com/angmu/MGLove/blob/master/Screenshot/点击tabBar.gif)
+![](Screenshot/点击tabBar.gif)
 
 
 - [x] 点击状态栏滚动到最前面
 
-![](https://github.com/angmu/MGLove/blob/master/Screenshot/点击状态栏滚动到最前面.gif)
+![](Screenshot/点击状态栏滚动到最前面.gif)
 
 - [x] 评论界面
 
-![](https://github.com/angmu/MGLove/blob/master/Screenshot/评论.gif)
+![](Screenshot/评论.gif)
 
 - [x] 登录界面
 
-![](https://github.com/angmu/MGLove/blob/master/Screenshot/登录界面.gif)
+![](Screenshot/登录界面.gif)
--- 百思不得姐/Classes/Publish-发布/Controller/MGPublishView.m
+++ 百思不得姐/Classes/Publish-发布/Controller/MGPublishView.m
@@ -7,17 +7,16 @@
 //
 
 #import "MGPublishView.h"
+#import "MGLoginTool.h"
+#import "MGNavigationController.h"
+#import "MGPostWordViewController.h"
 #import "MGVerticalButton.h"
 #import <pop/POP.h>
-#import "MGPostWordViewController.h"
-#import "MGNavigationController.h"
-#import "MGLoginTool.h"
 
 #define MGRootView [UIApplication sharedApplication].keyWindow.rootViewController.view
 
 @interface MGPublishView ()
 @end
-
 
 static CGFloat const MGAnimationDelay = 0.12;
 static CGFloat const MGAnimationSpeed = 10;
@@ -25,45 +24,41 @@ static CGFloat const MGAnimationSpeed = 10;
 @implementation MGPublishView
 
 /** 类方法 快速返回一个view */
-+ (instancetype)publishView
-{
++ (instancetype)publishView {
     return [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass(self) owner:nil options:nil] lastObject];
 }
 
 // 不用全局变量, 窗口一创建就销毁了
 static UIWindow *window_;
-+ (void)show
-{
++ (void)show {
     window_ = [[UIWindow alloc] init];
     window_.frame = [UIScreen mainScreen].bounds;
     window_.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.95];
     window_.hidden = NO;
-    
+
     MGPublishView *publish = [MGPublishView publishView];
     publish.frame = window_.bounds;
     [window_ addSubview:publish];
 }
 
-- (void)awakeFromNib
-{
+- (void)awakeFromNib {
     // 让控制器view不能点击
     self.userInteractionEnabled = NO;
-//    MGRootView.userInteractionEnabled = NO;
-    
+    //    MGRootView.userInteractionEnabled = NO;
+
     // 数据
     NSArray *images = @[@"publish-video", @"publish-picture", @"publish-text", @"publish-audio", @"publish-review", @"publish-offline"];
     NSArray *titles = @[@"发视频", @"发图片", @"发段子", @"发声音", @"审帖", @"离线下载"];
-    
+
     // 中间6个按钮
     CGFloat buttonW = 72;
     CGFloat buttonH = buttonW + 30;
-    CGFloat buttonStarY = (MGScreenH - 2*buttonH) * 0.5;
+    CGFloat buttonStarY = (MGScreenH - 2 * buttonH) * 0.5;
     CGFloat buttonStatX = 15;
     int maxCol = 3;
-    CGFloat xMargin = (MGScreenW - 2*buttonStatX - maxCol * buttonW) / (maxCol -1);
-    
-    for (int i = 0; i< images.count; i++) {
-        
+    CGFloat xMargin = (MGScreenW - 2 * buttonStatX - maxCol * buttonW) / (maxCol - 1);
+
+    for (int i = 0; i < images.count; i++) {
         MGVerticalButton *button = [[MGVerticalButton alloc] init];
         button.tag = i;
         [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
@@ -73,30 +68,30 @@ static UIWindow *window_;
         [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [button setImage:[UIImage imageNamed:images[i]] forState:UIControlStateNormal];
         button.titleLabel.font = [UIFont systemFontOfSize:14];
-        
+
         // 计算x\y
         int row = i / maxCol;
         int col = i % maxCol;
         CGFloat buttonX = buttonStatX + (buttonW + xMargin) * col;
         CGFloat buttonEndY = buttonStarY + buttonH * row;
         CGFloat buttonBeginY = buttonEndY - MGScreenH;
-        
+
         // 按钮动画
         POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewFrame];
-        
+
         anim.fromValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonBeginY, buttonW, buttonH)];
         anim.toValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonEndY, buttonW, buttonH)];
         anim.springSpeed = 10;
         anim.springBounciness = 10;
         anim.beginTime = CACurrentMediaTime() + MGAnimationDelay * i;
-        
+
         [button pop_addAnimation:anim forKey:nil];
     }
-    
+
     // 添加标语
     UIImageView *sloganView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"app_slogan"]];
     [self addSubview:sloganView];
-    
+
     // 标语动画
     POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewCenter];
     CGFloat centerX = MGScreenW * 0.5;
@@ -104,7 +99,7 @@ static UIWindow *window_;
     CGFloat centerBeginY = centerEndY - MGScreenH;
     sloganView.centerY = centerBeginY;
     sloganView.centerX = centerX;
-    
+
     anim.fromValue = [NSValue valueWithCGPoint:CGPointMake(centerX, centerBeginY)];
     anim.toValue = [NSValue valueWithCGPoint:CGPointMake(centerX, centerEndY)];
     anim.beginTime = CACurrentMediaTime() + images.count * MGAnimationDelay;
@@ -113,30 +108,28 @@ static UIWindow *window_;
     [anim setCompletionBlock:^(POPAnimation *anim, BOOL finished) {
         // 标语动画完成, 恢复点击事件
         self.userInteractionEnabled = YES;
-//        MGRootView.userInteractionEnabled = YES;
+        //        MGRootView.userInteractionEnabled = YES;
     }];
-    
+
     [sloganView pop_addAnimation:anim forKey:nil];
 }
 
 #pragma mark - 按钮点击 退出 弹出新的控制器
-- (void)buttonClick:(UIButton *)button
-{
+- (void)buttonClick:(UIButton *)button {
     [self cancelWithCompletionBlock:^{
-        
-//        if (button.tag == 0) {
-//            MGLog(@"发视频");
-//        } else if (button.tag == 1) {
-//            MGLog(@"发图片");
-//        }
+
+        //        if (button.tag == 0) {
+        //            MGLog(@"发视频");
+        //        } else if (button.tag == 1) {
+        //            MGLog(@"发图片");
+        //        }
         if (button.tag == 2) {
-            
             // 判断是否登录
-//            if([MGLoginTool getUid:YES] == nil) return;
-            
+            //            if([MGLoginTool getUid:YES] == nil) return;
+
             MGPostWordViewController *postWord = [[MGPostWordViewController alloc] init];
             MGNavigationController *nav = [[MGNavigationController alloc] initWithRootViewController:postWord];
-            
+
             UIViewController *root = [UIApplication sharedApplication].keyWindow.rootViewController;
             [root presentViewController:nav animated:YES completion:nil];
         }
@@ -145,22 +138,19 @@ static UIWindow *window_;
 
 #pragma mark - 取消
 - (IBAction)cancel {
-    
     [self cancelWithCompletionBlock:nil];
 }
 
 /**
  *  先执行退出动画，再执行动画完毕后的操作
  */
-- (void)cancelWithCompletionBlock:(void (^)())completionBlock
-{
+- (void)cancelWithCompletionBlock:(void (^)())completionBlock {
     // 让控制器view不能点击
     self.userInteractionEnabled = NO;
-//    MGRootView.userInteractionEnabled = NO;
-    
+    //    MGRootView.userInteractionEnabled = NO;
+
     int beginIndex = 1;
-    for (int i = beginIndex; i< self.subviews.count; i++) {
-        
+    for (int i = beginIndex; i < self.subviews.count; i++) {
         UIView *subView = self.subviews[i];
         // 基本动画
         POPBasicAnimation *anim = [POPBasicAnimation animationWithPropertyNamed:kPOPViewCenter];
@@ -172,14 +162,13 @@ static UIWindow *window_;
         // 动画执行节奏(开始很慢,后来很快)
         anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
         [subView pop_addAnimation:anim forKey:nil];
-        
+
         // 监听最后一个动画
         if (i == self.subviews.count - 1) {
-            
             [anim setCompletionBlock:^(POPAnimation *anim, BOOL finished) {
 
-//                MGRootView.userInteractionEnabled = YES;
-//                [self removeFromSuperview];
+                //                MGRootView.userInteractionEnabled = YES;
+                //                [self removeFromSuperview];
                 // 销毁窗口
                 window_.hidden = YES;
                 window_ = nil;
@@ -189,14 +178,13 @@ static UIWindow *window_;
                 //            }
 
                 // 装逼写法
-                !completionBlock ? :completionBlock();
+                !completionBlock ?: completionBlock();
             }];
         }
     }
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self cancelWithCompletionBlock:nil];
 }
 @end
